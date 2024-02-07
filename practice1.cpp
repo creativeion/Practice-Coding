5ab917adbfd4d23cf062b7f8f4674615ffbc6f0a
Ayan Agrawal <ayan.agrawal.aa@gmail.com>
2024-02-07 13:16:48 +0530

    First commit

---
+++ practice1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class A{
+    public:
+    void Display(){
+        cout<<"Hello world";
+    }
+    
+};
+
+int main(){
+    A a;
+    a.Display();
+    return 0;
+}
